79b2d74a05e73e72baa22b5b6a2af5a091f362b4
antikun <121240743+antikun@users.noreply.github.com>
2023-01-11 23:40:35 +0200

    Write most of the code for the "app"
    I think I've created an infinite while loop... does not work as expected.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-const commandPrompt = prompt("What would you like to do?");
+let command = prompt("What would you like to do?");
+
+let toDo = [];
+
+while (command !== "quit" || command !== null) {
+    if (command === "new") {
+        command = prompt("Enter new todo");
+        toDo.push(command);
+        console.log(`${toDo} added to list`);
+    } else if (command === "list") {
+        console.log("*************")
+        for (i = 0; i < toDo.length; i++) {
+            console.log(`${i}: ${toDo[i]}`);
+        }
+        console.log("*************")
+    } else if (command === "delete") {
+        command = prompt("Enter index of todo to remove");
+        toDo.splice(command, 1);
+        console.log("Todo removed");
+    }
+    command = prompt("What would you do?");
+}
+console.log("OK, YOU QUIT THE APP");
